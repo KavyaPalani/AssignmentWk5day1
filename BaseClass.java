10471b47270d829e79b1a00c8d24e12feddf60e0
KavyaPalani <100017576+KavyaPalani@users.noreply.github.com>
2022-03-25 22:24:42 +0530

    Add files via upload

---
+++ BaseClass.java
@@ -0,0 +1,29 @@
+package week5.day1.assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public  ChromeDriver driver;
+	@BeforeMethod
+	public void beforeMethod(){
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		driver.close();
+	}
+
+}
---
+++ CreateLead.java
@@ -0,0 +1,21 @@
+package week5.day1.assignment;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass {
+	@Test
+	public void test1() {
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+}
+
